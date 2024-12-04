bb08b666b34fe4298efe92b0cf92ba1b9b365a9b
Matias Klemola <matias.klemola@gmail.com>
2024-12-04 14:30:43 +0200

    Solve Day 3, part two

--- day3/solution.clj
+++ day3/solution.clj
@@ -8,3 +8,23 @@
                (re-seq regexp input))))
 
 (println (add-mul-ops (slurp "input.txt")))
+
+;; Part two, conditional instructions
+
+(defn process-instruction [[enabled? results] [_ do dont mul x y]]
+  (cond
+    do [true results]
+    dont [false results]
+    mul (if enabled?
+          [enabled? (cons (* (Integer/parseInt x) (Integer/parseInt y)) results)]
+          [enabled? results])
+    :else [enabled? results]))
+
+(defn add-conditional-mul-ops [input]
+  (->> input
+       (re-seq #"(do\(\))|(don't\(\))|(mul\((\d+),(\d+)\))")
+       (reduce process-instruction [true []])
+       second
+       (apply +)))
+
+(println (add-conditional-mul-ops (slurp "input.txt")))
