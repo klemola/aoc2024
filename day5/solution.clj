e3f507b7f503085e0714800feda18c66b83eeb5f
Matias Klemola <matias.klemola@gmail.com>
2024-12-10 14:08:15 +0200

    Solve Day 5, part two

--- day5/solution.clj
+++ day5/solution.clj
@@ -10,6 +10,16 @@
 (defn middle-number [nums]
   (nth nums (quot (count nums) 2)))
 
+(defn parse-input [input]
+  (let [[order-str pages-str] (str/split input #"\n\n")
+        order-tuples (map (fn [s] (mapv Integer/parseInt (str/split s #"\|"))) (str/split-lines order-str))
+        order-map (create-order-map order-tuples)
+        pages (->>
+               pages-str
+               str/split-lines
+               (mapv (fn [nums-str] (mapv Integer/parseInt (str/split nums-str #",")))))]
+    [order-map pages]))
+
 ;; Part one
 
 (defn validate-page [order-map page]
@@ -18,16 +28,25 @@
           (not (is-before current (first rest) order-map)) false
           :else (recur rest))))
 
-(defn count-pages [input]
-  (let [[order-str pages-str] (str/split input #"\n\n")
-        order-tuples (map (fn [s] (mapv Integer/parseInt (str/split s #"\|"))) (str/split-lines order-str))
-        order-map (create-order-map order-tuples)
-        pages (str/split-lines pages-str)]
+(defn count-valid-pages [input]
+  (let [[order-map, pages] (parse-input input)]
     (->>
      pages
-     (mapv (fn [nums-str] (mapv Integer/parseInt (str/split nums-str #","))))
      (filterv (partial validate-page order-map))
      (map middle-number)
      (apply +))))
 
-(println (count-pages (slurp "input.txt")))
+(println (count-valid-pages (slurp "input.txt")))
+
+;; Part two
+
+(defn count-invalid-sorted-pages [input]
+  (let [[order-map, pages] (parse-input input)]
+    (->>
+     pages
+     (filter (complement (partial validate-page order-map)))
+     (map (partial sort (fn [a b] (is-before b a order-map))))
+     (map middle-number)
+     (apply +))))
+
+(println (count-invalid-sorted-pages (slurp "input.txt")))
